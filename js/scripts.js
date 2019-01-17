a1ef95ef0d859f8fccf59ab374cb7dfcf9ca21b7
Ryan Dikeos and Tracy Jo Reith <ryan@>
2019-01-16 16:40:53 -0800

    Javascript with jQuery to take in put from survey and output into a welcome message

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+$(document).ready(function() {
+  $("#welcomeForm").submit(function(event) {
+    var prefName = $("input#prefName").val();
+    var favColor = $("#favColor").val();
+    var dob = $("#dob").val();
+    var vehMake = $("input:radio[name=vehMake]:checked").val();
+    var computer = $("#computer").val();
+    $(".prefName").text(prefName);
+    $(".dob").text(dob);
+    $(".computer").text(computer);
+    $(".favColor").text(favColor);
+    $(".vehMake").text(vehMake);
+    $(".container").children().toggle();
+    event.preventDefault();
+  });
+});
